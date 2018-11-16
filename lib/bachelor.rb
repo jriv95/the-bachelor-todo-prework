d6868d5712d94bb1719a26b65ee1dabb1f945061
Joel Rivera <joel.rivera7995@gmail.com>
2018-11-16 20:23:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,19 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
-  data.each do |season_num, contestant|
-    contestant.each{|categories|}
+  data[season.to_sym].each do |contestant|
+    if contestant[:status] == "Winner"
+      return contestant[:name].split(" ")[0
+      end]
   end
+end
+
+def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
+ end
 end
 
 def get_contestant_name(data, occupation)
