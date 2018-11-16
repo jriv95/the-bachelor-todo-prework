9a92c6722402d637736d22b2d210d36653e752bf
Joel Rivera <joel.rivera7995@gmail.com>
2018-11-16 20:17:35 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num, contestant|
+    contestant.each{|categories|}
+  end
 end
 
 def get_contestant_name(data, occupation)
